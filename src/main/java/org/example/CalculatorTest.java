b067aa50fb2b99316c59925f0da9eaf46d578d59
mehdixbachir-cmyk <mehdixbachir@gmail.com>
2025-12-06 22:55:54 +0100

    Add Calculator class + JUnit test

---
+++ src/main/java/org/example/CalculatorTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, Calculator.add(2, 3));
+    }
+}
+
