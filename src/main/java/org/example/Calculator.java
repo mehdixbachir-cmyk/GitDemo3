247806f6f481a4770a51ff73cb6241af98b6a76c
mehdixbachir-cmyk <mehdixbachir@gmail.com>
2025-12-06 23:02:04 +0100

    Add multiply method + test

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,6 +4,11 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+    // Calculator.java
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Hello GitDemo3!");
