e8d5c6eb6f0b35130c447e31f631b8bdda1ad30a
mehdixbachir-cmyk <mehdixbachir@gmail.com>
2025-12-06 23:29:00 +0100

    add division methode+JUbit test

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,6 +8,11 @@ public class Calculator {
     public static int multiply(int a, int b) {
         return a * b;
     }
+    public static int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
+
 
 
     public static void main(String[] args) {
